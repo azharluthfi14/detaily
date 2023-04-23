6260bc40af66c1c65daabbe4305768b94568d6cc
azharluthfi14 <35468624+azharluthfi14@users.noreply.github.com>
2023-04-23 13:30:59 +0700

    Fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,16 +11,11 @@ module.exports = {
   plugins: ['react-refresh', 'unused-imports', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-
     'no-unused-vars': 'off',
     'no-console': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react/no-unescaped-entities': 'off',
     'react/display-name': 'off',
-    'react/jsx-curly-brace-presence': [
-      'warn',
-      { props: 'never', children: 'never' },
-    ],
   },
   globals: {
     React: true,
